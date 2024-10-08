a8e259bf643fe54387bcb48f6acba6ef9b92d63a
francodegano <francodegano2@gmail.com>
2024-10-07 23:34:52 -0300

    Stretch furniture

--- MrTest/Assets/PrefabToReality.cs
+++ MrTest/Assets/PrefabToReality.cs
@@ -18,22 +18,40 @@ public class PrefabToReality : AnchorPrefabSpawner
             }
         }
 
-        Bounds? anchor_bound = anchor.VolumeBounds;
-        GameObject closest = pref_renders[0];
-        float smalles_dif = Mathf.Infinity;
-
-        for(int i=0;i<pref_renders.Count;i++)
+        Bounds? nil_anchor_bound = anchor.VolumeBounds;
+        if(nil_anchor_bound.HasValue)
         {
-            var bounds = Utilities.GetPrefabBounds(pref_renders[i]);
-            float diff = Mathf.Abs((float)(anchor_bound?.size.magnitude - bounds?.size.magnitude));
-            if(smalles_dif > diff)
+            Bounds anchor_bound = nil_anchor_bound.Value;
+            GameObject closest = prefabs[0];
+            float smalles_dif = Mathf.Infinity;
+
+            Debug.Log("anchor" + anchor_bound.size);
+
+            for(int i=0;i<pref_renders.Count;i++)
             {
-                smalles_dif = diff;
-                closest = prefabs[i];
+                var a = pref_renders[i];
+                var b = a.GetComponent<Renderer>();
+                var c = b.bounds;
+                Debug.Log("bounds "+ c.size);
+                var bounds = Utilities.GetPrefabBounds(pref_renders[i]);
+                Debug.Log("Utility bounds" + bounds);
+                if(!bounds.HasValue)
+                {
+                    continue;
+                }
+                float diff = Mathf.Abs((float)(anchor_bound.size.magnitude - bounds.Value.size.magnitude));
+                Debug.Log("Diff " + i + " " + diff);
+                if(smalles_dif > diff)
+                {
+                    smalles_dif = diff;
+                    closest = prefabs[i];
+                }
             }
+            return closest;
         }
 
 
-        return closest;
+        return null;
+        
     }
 }
